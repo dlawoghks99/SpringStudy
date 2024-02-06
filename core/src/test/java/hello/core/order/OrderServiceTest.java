1e483d9cc1ed14a1535fe4a4931a37a3264e64e7
JeaHwanLim <dlawoghks5708@gmail.com>
2024-02-06 14:52:28 +0900

    의존성 주입 소스코드

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,36 @@
+package hello.core.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import hello.core.AppConfig;
+import hello.core.discount.Order;
+import hello.core.discount.OrderService;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+
+public class OrderServiceTest {
+	
+	AppConfig appConfig = new AppConfig();
+	
+	MemberService memberService = appConfig.memberService();
+	OrderService orderService = appConfig.orderService();
+	
+//	MemberService memberService = new MemberServiceImpl();
+//	OrderService orderService = new OrderServiceImpl();
+	
+	@Test
+	void createOrder() {
+		Long memberId = 1L;
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		
+		memberService.join(member);
+		
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+		
+		Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+		
+	}
+	
+}
